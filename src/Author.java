08f9c3d8cb790d26d9644a14a000c595d960cfaf
heather arellano <hma.485@icloud.com>
2018-05-14 15:01:00 -0500

    Commit

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,4 @@
+
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
@@ -16,19 +17,20 @@ public class Author extends Person {
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+    @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
-
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
 
-    @Override
+
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
